fb55551c07ec33cfc81ea3d143c6a17aba6ef600
Sadko-o <ruana.saduakhas@nu.edu.kz>
2022-06-15 15:00:53 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import mountain from './img/mountain.jfif'
+import river from './img/river.jpg'
+import mpath from './img/mpath.jfif'
+import fox from './img/fox.jfif'
+import sky from './img/sky.jfif'
+import icon from './img/icons.png'
+import { format } from 'date-fns'
+import React from 'react'
 
+const currentTime = format(new Date(),'dd.mm.yyyy hh:mm');
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <div className="header">
+            <img src={icon} alt="new"/>
+            <input type="text"/>           
+          <div className='time' >{currentTime}</div>
+          
+      </div>
+
+      <div className='row'>
+      <div className='col'> 
+        <div className='centered'>Hey</div>
+        <img src={mountain} alt="new"/>
+      </div>
+      <div className='col'> 
+        <div className='centered'>Let's</div>
+        <img src={river} alt="new"/>
+        </div>
+      <div className='col'> 
+        <div className='centered'>Give</div>
+        <img src={mpath} alt="new"/>
+        </div>
+      <div className='col'>
+        <div className='centered'>All</div>
+        <img src={fox} alt="new"/>
+      </div>
+      <div className='col'> 
+        <div className='centered'>You can</div>
+        <img src={sky} alt="new"/></div>
+      </div>
+
+      </div>
   );
 }
 
